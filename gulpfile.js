7dc30201026b82f43f391dbc93769172f7afac25
Barnz <hi@barnz.dev>
2021-11-17 21:27:43 +0000

    install and setup

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+const _PROD = process.env.MODE === 'prod' ? true : false;
+
+const { src, dest, watch, series, parallel } = require('gulp');
+const gulpif = require('gulp-if');
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const del = require('del');
+
+const outputLocation = './dist';
+const sassLocation = './src/css/main.scss';
+const htmlLocation = './src/html/index.html';
+const assetLocation = './src/assets/**/*.*';
+
+function cleanup() {
+    return del([outputLocation + '/**/*']);
+}
+
+function compileSass() {
+    return src(sassLocation)
+        .pipe(gulpif(!_PROD, sourcemaps.init()))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(
+            autoprefixer({
+                cascade: false,
+            })
+        )
+        .pipe(cleanCSS())
+        .pipe(gulpif(!_PROD, sourcemaps.write('.')))
+        .pipe(dest(outputLocation));
+}
+
+function buildHtml() {
+    return src(htmlLocation).pipe(dest(outputLocation));
+}
+
+function copyAssets() {
+    return src(assetLocation).pipe(dest(outputLocation));
+}
+
+function watchSource() {
+    watch('./src/css/**/*.scss', series(compileSass, buildHtml));
+    watch(htmlLocation, buildHtml);
+}
+
+exports.build = series(cleanup, parallel(copyAssets, compileSass), buildHtml);
+exports.watch = series(
+    cleanup,
+    copyAssets,
+    compileSass,
+    buildHtml,
+    watchSource
+);
+exports.clean = cleanup;
