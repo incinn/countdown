a1c247e06063a03d52eda69c667f687d03f4ca37
Barnz <hi@barnz.dev>
2021-11-29 21:31:45 +0000

    initial typescript compilation

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const _PROD = process.env.MODE === 'prod' ? true : false;
 
 const { src, dest, watch, series, parallel } = require('gulp');
+const Dotenv = require('dotenv-webpack');
 const gulpif = require('gulp-if');
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass')(require('sass'));
@@ -9,10 +10,14 @@ const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const del = require('del');
 
+const webpack = require('webpack');
+const webpackStream = require('webpack-stream');
+
 const outputLocation = './dist';
 const sassLocation = './src/css/main.scss';
 const htmlLocation = './src/html/index.html';
 const assetLocation = './src/assets/**/*.*';
+const tsLocation = './src/js/**/*.ts';
 
 function cleanup() {
     return del([outputLocation + '/**/*']);
@@ -32,6 +37,43 @@ function compileSass() {
         .pipe(dest(outputLocation));
 }
 
+function compileTs() {
+    return src(tsLocation)
+        .pipe(
+            webpackStream({
+                mode: _PROD ? 'production' : 'development',
+                devtool: _PROD ? '' : 'source-map',
+                entry: './src/js/main.ts',
+                module: {
+                    rules: [
+                        {
+                            test: /\.tsx?$/,
+                            use: 'ts-loader',
+                            exclude: /node_modules/,
+                        },
+                    ],
+                },
+                plugins: [
+                    new Dotenv({
+                        path: './.env',
+                    }),
+                    new webpack.DefinePlugin({
+                        __VERSION: JSON.stringify(
+                            require('./package.json').version
+                        ),
+                    }),
+                ],
+                resolve: {
+                    extensions: ['.tsx', '.ts', '.js'],
+                },
+                output: {
+                    filename: 'bundle.js',
+                },
+            })
+        )
+        .pipe(dest(outputLocation));
+}
+
 function buildHtml() {
     return src(htmlLocation).pipe(dest(outputLocation));
 }
@@ -41,15 +83,21 @@ function copyAssets() {
 }
 
 function watchSource() {
-    watch('./src/css/**/*.scss', series(compileSass, buildHtml));
+    watch('./src/css/**/*.scss', compileSass);
+    watch('./src/js/**/*.ts', compileTs);
     watch(htmlLocation, buildHtml);
 }
 
-exports.build = series(cleanup, parallel(copyAssets, compileSass), buildHtml);
+exports.build = series(
+    cleanup,
+    parallel(copyAssets, compileSass, compileTs),
+    buildHtml
+);
 exports.watch = series(
     cleanup,
     copyAssets,
     compileSass,
+    compileTs,
     buildHtml,
     watchSource
 );
---
+++ src/js/components/_timer.ts
@@ -0,0 +1,7 @@
+export class Timer {
+    constructor() {}
+
+    public init(): void {
+        console.log('timer started');
+    }
+}
