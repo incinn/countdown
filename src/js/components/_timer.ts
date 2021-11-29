3a21054163c37b3ba02ecbf4c534028383d43535
Barnz <hi@barnz.dev>
2021-11-29 21:48:48 +0000

    wip timer

--- src/js/components/_timer.ts
+++ src/js/components/_timer.ts
@@ -1,7 +1,30 @@
+import * as dayjs from 'dayjs';
+import * as relativeTime from 'dayjs/plugin/relativeTime';
+
 export class Timer {
-    constructor() {}
+    belgiumDate = '29 December 2021 12:05 GMT';
+
+    constructor() {
+        dayjs.extend(relativeTime);
+    }
 
     public init(): void {
         console.log('timer started');
+
+        this.calculate();
+    }
+
+    private calculate(): void {
+        // https://stackoverflow.com/questions/66639760/dayjs-diff-between-two-date-in-day-and-hours
+        const date1 = dayjs();
+        const date2 = dayjs(this.belgiumDate);
+        const diff = date2.diff(date1, 'day', true);
+
+        console.log('obtained', diff);
+
+        const days = Math.floor(diff);
+        const hours = Math.floor((diff - days) * 24);
+
+        console.log(`${days} days, ${hours} hours`);
     }
 }
