7c6ecdaea665fc34b6af40555dfc3b72d2f9ea91
Barnz <hi@barnz.dev>
2021-12-02 19:22:25 +0000

    add package.json version number to footer

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,7 +1,11 @@
 import { DateEl } from './components/_date';
 import { Timer } from './components/_timer';
+declare var __VERSION: string;
 
 window.onload = () => {
     new Timer().init();
     new DateEl().init();
+
+    const versionEl = document.getElementById('version');
+    versionEl.innerHTML = __VERSION;
 };
