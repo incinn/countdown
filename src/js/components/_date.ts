d645d69a4b17acbf3ac317cd86326ec09ae580f9
Barnz <hi@barnz.dev>
2021-12-02 19:21:55 +0000

    add initial date event element

---
+++ src/js/components/_date.ts
@@ -0,0 +1,55 @@
+import dayjs, { Dayjs } from 'dayjs';
+
+const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sept',
+    'Oct',
+    'Nov',
+    'Dec',
+];
+
+export class DateEl {
+    private date: Dayjs;
+    private dateEl: HTMLElement;
+
+    constructor() {
+        this.dateEl = document.getElementById('date');
+
+        if (!this.dateEl) {
+            console.error('Unable to start date');
+            this.init = () => {};
+        }
+    }
+
+    public init(): void {
+        this.updateDate();
+    }
+
+    private getDate(): void {
+        this.date = dayjs('29 December 2021 12:05 GMT');
+    }
+
+    private updateDate(): void {
+        this.getDate();
+
+        const day = this.dateEl.querySelector('span.day');
+        const month = this.dateEl.querySelector('span.month');
+        const year = this.dateEl.querySelector('span.year');
+
+        if (!day || !month || !year) {
+            console.error('Unable to find date elements');
+            return;
+        }
+
+        day.innerHTML = this.date.date().toString();
+        month.innerHTML = months[this.date.month()];
+        year.innerHTML = this.date.year().toString();
+    }
+}
--- src/js/main.ts
+++ src/js/main.ts
@@ -1,5 +1,7 @@
+import { DateEl } from './components/_date';
 import { Timer } from './components/_timer';
 
 window.onload = () => {
     new Timer().init();
+    new DateEl().init();
 };
