accd16ac62352281b879ef582152967087ea1486
Barnz <hi@barnz.dev>
2021-12-08 20:38:22 +0000

    add public function to plugin class to return localStorage timer data

--- src/js/components/_plugin.ts
+++ src/js/components/_plugin.ts
@@ -1,3 +1,6 @@
+import { TimerConfetti } from '../models/confetti.model';
+import { TargetTimer } from '../models/targetTimer.model';
+
 export class SitePlugin {
     constructor() {}
 
@@ -5,5 +8,19 @@ export class SitePlugin {
 
     public getDate(): string {
         return localStorage.getItem('targetDate');
+    }
+
+    public getTimerData(): TargetTimer {
+        return {
+            date: this.getDate(),
+            specialNumber: parseInt(localStorage.getItem('specialNumber')),
+            specialNumberConfetti: JSON.parse(
+                localStorage.getItem('specialNumberConfetti')
+            ) as TimerConfetti,
+            successText: '',
+            successConfetti: JSON.parse(
+                localStorage.getItem('successConfetti')
+            ) as TimerConfetti,
+        };
     }
 }
