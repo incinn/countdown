b1f62b1e76101bea2b90196e5560329537b4c157
barnz <hi@barnz.dev>
2022-08-18 19:32:00 +0100

    display time since timer completed

--- src/js/components/_timer.ts
+++ src/js/components/_timer.ts
@@ -6,117 +6,120 @@ import { SitePlugin } from './_plugin';
 import { TargetTimer } from '../models/targetTimer.model';
 
 interface CountdownTimer {
-    years: number;
-    months: number;
-    days: number;
-    hours: number;
-    minutes: number;
-    seconds: number;
+  years: number;
+  months: number;
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
 }
 
 export class Timer extends SitePlugin {
-    private countdownEl: HTMLElement;
-    private _timer: any;
-    private confetti: JSConfetti;
-    private data: TargetTimer;
+  private countdownEl: HTMLElement;
+  private successTextEl: HTMLElement;
+  private _timer: any;
+  private confetti: JSConfetti;
+  private data: TargetTimer;
+  private successMode = false;
 
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        dayjs.extend(relativeTime);
-        dayjs.extend(duration);
+    dayjs.extend(relativeTime);
+    dayjs.extend(duration);
 
-        this.countdownEl = document.getElementById('timer');
-        this.confetti = new JSConfetti();
+    this.countdownEl = document.getElementById('timer');
+    this.successTextEl = document.getElementById('successText');
+    this.confetti = new JSConfetti();
 
-        if (!this.countdownEl || !this.confetti) {
-            console.error('Unable to start timer');
-            // eslint-disable-next-line @typescript-eslint/no-empty-function
-            this.init = () => {};
-        }
+    if (!this.countdownEl || !this.successTextEl || !this.confetti) {
+      console.error('Unable to start timer');
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      this.init = () => { };
+    }
+  }
+
+  public init(): void {
+    this.successMode = false;
+    this.data = this.getTimerData();
+
+    this._timer = setInterval(() => this.updateTimer(), 1000);
+    this.updateTimer();
+  }
+
+  public rebuild(): void {
+    clearInterval(this._timer);
+    this.successMode = false;
+    this.successTextEl.innerHTML = '';
+    this.init();
+  }
+
+  private updateTimer(): void {
+    const timer = this.calculate(this.data.date);
+
+    let output = '';
+
+    if (timer.years > 0 || timer.years < -0.1) output += this.formatSegment(timer.years, 'year');
+    if (timer.months > 0 || timer.months < -0.1)
+      output += this.formatSegment(timer.months, 'month');
+    if (timer.days > 0 || timer.days < -0.1) output += this.formatSegment(timer.days, 'day');
+    if (timer.hours > 0 || timer.hours < -0.1) output += this.formatSegment(timer.hours, 'hour');
+    output += this.formatSegment(timer.minutes, 'minute');
+    output += this.formatSegment(timer.seconds, 'second');
+
+    if (this.isSuccess(timer)) {
+      output += `<div class="segment">ago</div>`;
+      this.handleSuccess();
     }
 
-    public init(): void {
-        this.data = this.getTimerData();
+    this.countdownEl.innerHTML = output.trim();
+  }
 
-        this._timer = setInterval(() => this.updateTimer(), 1000);
-        this.updateTimer();
+  private isSuccess(timer: CountdownTimer): boolean {
+    console.log('success mins', timer.minutes);
+    console.log('success seconds', timer.seconds);
+    return timer.minutes <= 0 && timer.seconds < 0;
+  }
+
+  private handleSuccess(): void {
+    this.confetti.addConfetti(this.data.successConfetti);
+
+    if (!this.successMode) {
+      this.successTextEl.innerHTML = this.data.successText;
+      this.successMode = true;
     }
+  }
 
-    public rebuild(): void {
-        clearInterval(this._timer);
-        this.init();
-    }
+  private calculate(target: string): CountdownTimer {
+    const raw = dayjs
+      .duration(dayjs(target).diff(dayjs()))
+      .format('Y;M;D;H;m;s;');
 
-    private updateTimer(): void {
-        const timer = this.calculate(this.data.date);
+    const data = raw.split(';');
 
-        let output = '';
+    return {
+      years: +data[0],
+      months: +data[1],
+      days: +data[2],
+      hours: +data[3],
+      minutes: +data[4],
+      seconds: +data[5],
+    };
+  }
 
-        if (timer.years > 0) output += this.formatSegment(timer.years, 'year');
-        if (timer.months > 0)
-            output += this.formatSegment(timer.months, 'month');
-        if (timer.days > 0) output += this.formatSegment(timer.days, 'day');
-        if (timer.hours > 0) output += this.formatSegment(timer.hours, 'hour');
-        output += this.formatSegment(timer.minutes, 'minute');
-        output += this.formatSegment(timer.seconds, 'second');
+  private formatSegment(time: number, text: string): string {
+    time = Math.abs(time);
+    let output = '<div class="segment">';
 
-        this.countdownEl.innerHTML = output.trim();
+    if (time != 1) text += 's';
 
-        if (
-            timer.years <= 0 &&
-            timer.days <= 0 &&
-            timer.hours <= 0 &&
-            timer.minutes <= 0 &&
-            timer.seconds <= 0
-        ) {
-            const successTextEl = document.createElement('span');
-            successTextEl.classList.add('completed');
-            successTextEl.innerHTML = this.data.successText;
+    if (time === this.data.specialNumber) {
+      output += `<span class="highlight">${time}</span> `;
+      this.confetti.addConfetti(this.data.specialNumberConfetti);
+    } else output += `${time} `;
 
-            this.countdownEl.innerHTML = '';
-            this.countdownEl.appendChild(successTextEl);
-            this.timerComplete();
-        }
-    }
+    output += `<span class="text">${text}</span></div>`;
 
-    private calculate(target: string): CountdownTimer {
-        const raw = dayjs
-            .duration(dayjs(target).diff(dayjs()))
-            .format('Y;M;D;H;m;s;');
-
-        const data = raw.split(';');
-
-        return {
-            years: +data[0],
-            months: +data[1],
-            days: +data[2],
-            hours: +data[3],
-            minutes: +data[4],
-            seconds: +data[5],
-        };
-    }
-
-    private formatSegment(time: number, text: string): string {
-        let output = '<div class="segment">';
-
-        if (time != 1) text += 's';
-
-        if (time === this.data.specialNumber) {
-            output += `<span class="highlight">${time}</span> `;
-            this.confetti.addConfetti(this.data.specialNumberConfetti);
-        } else output += `${time} `;
-
-        output += `<span class="text">${text}</span></div>`;
-
-        return output;
-    }
-
-    private timerComplete(): void {
-        clearInterval(this._timer);
-        this._timer = setInterval(
-            () => this.confetti.addConfetti(this.data.successConfetti),
-            2200
-        );
-    }
+    return output;
+  }
 }
